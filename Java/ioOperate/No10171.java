ed064627dec14d164986c1cac8853f50aeb3201f
jskim <ewhdtls0@naver.com>
2022-03-27 20:29:22 +0900

    Solving 10171, 10718

---
+++ Java/ioOperate/No10171.java
@@ -0,0 +1,11 @@
+package Java.ioOperate;
+
+public class No10171 {
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+        
+    }
+}
